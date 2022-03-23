2c5bbc7867f7ed92bab5a77ffb8b49cf901f0248
leska-ver <lyaisan.333@yandex.ru>
2022-03-23 15:14:07 +0500

    cedro

--- js/cedro.js
+++ js/cedro.js
@@ -145,13 +145,13 @@ document.addEventListener('DOMContentLoaded', function () {
     },
     spaceBetween: 20,
 
-    //Бесконечное листание страниц
-    speed: 2000,//Интервал ожидания
+     //Бесконечное листание страниц
+     speed: 2000, //Интервал ожидания
 
-    autoplay: {
-      delay: 3000,//Интервал ожидания
+     autoplay: {
+       delay: 3000,//Интервал ожидания
          disableOnInteraction: false,      
-    }, 
+     }, 
 
     pagination: {
       el: ".slider__section .slider__pagination",
